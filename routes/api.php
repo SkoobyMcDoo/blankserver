92d168dbc8e16883967f9cce623e6e949502c603
Shuaib <shuaib.rangraje@gmail.com>
2019-08-06 12:14:12 +0000

    Auth API added

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Auth API
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\APIController@login');
+    Route::post('signup', 'Auth\APIController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'Auth\APIController@logout');
+        Route::get('user', 'Auth\APIController@user');
+    });
 });
